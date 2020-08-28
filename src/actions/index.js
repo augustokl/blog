e595717d43f6c6817a431426dfdbffbbe4adb624
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-08-27 23:20:02 -0300

    add action to fetch users

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,8 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
   
-export const selectPost = () => {
-  return {
-    type: 'SELECT_POST'
-  }
-}
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USERS', payload: response.data });
+};
